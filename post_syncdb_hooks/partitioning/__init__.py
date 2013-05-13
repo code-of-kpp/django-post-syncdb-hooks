744fa8ee5f793f64896be6d2c8d5ae76bf239abf
Konstantin Ignatov <k.ignatov@neodialog.ru>
2013-05-13 17:37:37 +0400

    fix decorator -- NameError

--- post_syncdb_hooks/partitioning/__init__.py
+++ post_syncdb_hooks/partitioning/__init__.py
@@ -6,7 +6,8 @@ BROKEN_INSERT_RETURN = ('django.db.backends.postgresql_psycopg2', )
 def to_partition(func):
     """Decorator that wraps turning off `can_return_id_from_insert`
     db connection feature"""
-    if any((db['ENGINE'] in BROKEN_INSERT_RETURN for db in settings.DATABASES)):
+    if any(((settings.DATABASES[alias]['ENGINE'] in BROKEN_INSERT_RETURN)
+            for alias in settings.DATABASES)):
         def wrapper(using=DEFAULT_DB_ALIAS, *args, **kwargs):
             """{}
             Wrapped with
