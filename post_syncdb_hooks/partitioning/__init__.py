302e4797195524f016bf006b5513216fb4012be6
Konstantin Ignatov <k.ignatov@neodialog.ru>
2013-05-13 14:40:12 +0400

    decorator for save() currently to use with postgre

---
+++ post_syncdb_hooks/partitioning/__init__.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db import connections, DEFAULT_DB_ALIAS
+
+BROKEN_INSERT_RETURN = ('django.db.backends.postgresql_psycopg2', )
+
+def to_partition(func):
+    """Decorator that wraps turning off `can_return_id_from_insert`
+    db connection feature"""
+    if any((db['ENGINE'] in BROKEN_INSERT_RETURN for db in settings.DATABASES)):
+        def wrapper(using=DEFAULT_DB_ALIAS, *args, **kwargs):
+            """{}
+            Wrapped with
+            {}""".format(func.__doc__, to_partition.__doc__)
+            kwargs['using'] = using
+            if settings.DATABASES[using]['ENGINE'] in BROKEN_INSERT_RETURN:
+                oldval = connections[using].features.can_return_id_from_insert
+                connections[using].features.can_return_id_from_insert = False
+                return_value = func(*args, **kwargs)
+                connections[using].features.can_return_id_from_insert = oldval
+                return return_value
+            else:
+                return func(*args, **kwargs)
+        return wrapper
+    else:
+        return func
